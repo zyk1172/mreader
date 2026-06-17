babbc9d8fcb0716fff372d8f9d4867d11355f001
ZHENG YUNKAI <769440615@>
2026-06-17 21:37:45 +0800

    Initial Commit

---
+++ mreaderTests/mreaderTests.swift
@@ -0,0 +1,19 @@
+//
+//  mreaderTests.swift
+//  mreaderTests
+//
+//  Created by 郑云凯 on 2026/6/17.
+//
+
+import Testing
+@testable import mreader
+
+struct mreaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
